a089a60a38b988228fdcd99939534325f9a616e7
Mone't Fulgham <monetfulgham@gmail.com>
2025-05-13 16:03:48 -0400

    Finish search animation

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
 import { HiMagnifyingGlassCircle } from "react-icons/hi2";
+import { set } from "yaml/dist/schema/yaml-1.1/set";
 
 interface SearchProps {
   searchTerm: string;
   setSearchTerm: (searchTerm: string) => void;
   searchError: boolean;
-  setSearchError: (value: ((prevState: boolean) => boolean) | boolean) => void;
+  setSearchError: (boolean) => void;
   isSearchExpanded: boolean;
-  setSearchExpanded: (prevState: boolean) => boolean;
+  setSearchExpanded: (boolean) => void;
 }
 
 const Search = ({
@@ -22,7 +23,7 @@ const Search = ({
     setSearchTerm("");
     setSearchError(false);
   };
-
+  console.log({ isSearchExpanded });
   useEffect(() => {
     if (searchTerm === "" && searchError) {
       setSearchError(false);
@@ -31,31 +32,39 @@ const Search = ({
 
   return (
     <div className={"ml-4 flex justify-items-end"}>
-      {!isSearchExpanded ? (
-        <button
-          onClick={() => setSearchExpanded(true)}
-          className={"justify-self-center items-center"}
-        >
-          <HiMagnifyingGlassCircle
-            className={"w-[30px] h-[30px]"}
-            alt={"Search"}
-          />
-        </button>
-      ) : (
-        <>
-          <input
-            className={"border border-black mr-2 rounded-md h-[40px] p-2"}
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
-          />
-          <button
-            className={"text-[12px] ml-2 text-gray-500 hover:text-blue-600"}
-            onClick={handleReset}
-          >
-            Reset Search
-          </button>
-        </>
-      )}
+      <button
+        onClick={() => setSearchExpanded(!isSearchExpanded)}
+        className={"justify-self-center items-center mr-2"}
+      >
+        <HiMagnifyingGlassCircle
+          className={"w-[30px] h-[30px]"}
+          alt={"Search"}
+        />
+      </button>
+
+      <>
+        <input
+          className={`${isSearchExpanded ? "w-[200px] h-[40px] duration-500 " : "duration-500 w-[1px] h-[1px] border-none"} flex ease-in-out border border-black mr-2 rounded-md p-2`}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
+        />
+        {isSearchExpanded && (
+          <>
+            <button
+              className={"text-[12px] ml-2 text-gray-500 hover:text-blue-600"}
+              onClick={handleReset}
+            >
+              Reset Search
+            </button>
+            <button
+              className={"text-[12px] ml-3 text-gray-500 hover:text-blue-600"}
+              onClick={() => setSearchExpanded(false)}
+            >
+              Cancel
+            </button>
+          </>
+        )}
+      </>
       {searchError && (
         <div className={"text-[12px] text-red-600 mt-2"}>
           <p>No results found</p>
